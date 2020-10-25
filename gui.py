e4991872b8e02fbe9844b74e8793cc71a5d7d56e
Cian Crowley-Smith <cian.crowleysmith22@mail.dcu.ie>
2020-10-25 12:41:16 +0000

    added the main file to run entire program

--- gui.py
+++ gui.py
@@ -3,87 +3,100 @@ from tkinter import *
 from tkinter import Entry
 import ip_calculator as ipc
 
+def run():
 
-#helper functions
-def clear_radio():
-    c1.deselect()
-    c2.deselect()
-    c3.deselect()
 
-def process_input():
-    if r.get() == 1:
-        labels = []
-        ip = e1.get()
-        class_type, networks, hosts, firstAddress, lastAddress = ipc.get_class_stat(ip)
-        classLabel = Label(outputs, text='class: {}\nnetworks: {}\nhosts: {}\nfirst address: {}\nlast address: {}'.format(class_type, networks, hosts, firstAddress, lastAddress))
-        classLabel.grid(row=0, column=0)
-        labels.append(classLabel)
-    if r.get() == 2:
-        ip = e1.get()
-        subnet = e2.get()
-        address, nets,v_host, valid_subs, b_addrs, f_addr, l_addr = ipc.get_subnet_stats(ip, subnet)
-        label = Label(outputs, text="Address: {}\nSubnets: {}\nAddressable Hosts per subnet: {}\nValid Subnets: {}\n Broadcast Addresses: {}\nFirst Addresses: {}\nLast Addresses: {}".format(address, nets,v_host, valid_subs, b_addrs, f_addr, l_addr))
+    widgets = []
+
+    def process_input():
+        label = Label(outputs, text='Your Output Will appear here')
         label.grid(row=0, column=0)
+        widgets.append(label)
+        if r.get() == 1:
+            for l in widgets:
+                l.destroy()
 
-    if r.get == 3:
-        supernet_list = e3.get()
+            ip = e1.get()
+            class_type, networks, hosts, firstAddress, lastAddress = ipc.get_class_stat(ip)
+            label = Label(outputs, justify='left' ,text='class: {}\nnetworks: {}\nhosts: {}\nfirst address: {}\nlast address: {}'.format(class_type, 
+            networks, hosts, firstAddress, lastAddress))
+            label.grid(row=0, column=0)
+            widgets.append(label)
 
-# creating main tkinter window/toplevel 
-master = Tk()
-master.minsize(550, 550)
-master.title("Ip Assignment 1")
-master.iconphoto(False, PhotoImage(file='logo.png'))
+        if r.get() == 2:
+            for l in widgets:
+                l.destroy()
 
-master.columnconfigure(0, weight=1)
-master.rowconfigure(1, weight=1)
+            ip = e1.get()
+            subnet = e2.get()
+            address, nets,v_host, valid_subs, b_addrs, f_addr, l_addr = ipc.get_subnet_stats(ip, subnet)
+            label = Label(outputs, justify='left', text="Address: {}\nSubnets: {}\nAddressable Hosts per subnet: {}\nValid Subnets: {}\nBroadcast Addresses: {}\nFirst Addresses: {}\nLast Addresses: {}".format(address, 
+            nets,v_host, valid_subs, b_addrs, f_addr, l_addr))
+            label.grid(row=0, column=0)
+            widgets.append(label)
 
-#input frame
-inputs = LabelFrame(master, text="Main Menu", padx=5, pady=5)
-inputs.grid(row=0, column=0, padx=10, pady = 10, sticky = E+W+N+S, columnspan=4)
-inputs.rowconfigure(0, weight=1)
-inputs.columnconfigure(0, weight=1)
+        if r.get() == 3:
+            for l in widgets:
+                l.destroy()
+        
+            supernet_list = e3.get().split(",")
+            print(supernet_list)
+            supernet, netmask = ipc.get_supernet_stats(supernet_list)
+            print(supernet, netmask)
+            label = Label(outputs, justify='left' ,text="Address: {}\nNetwork Mask: {}".format(supernet, netmask))
+            label.grid(row=0, column=0)
+            widgets.append(label)
 
-#output frame
-outputs = LabelFrame(master, text="Output", padx=5, pady=5)
-outputs.grid(row=1, column=0, padx=10, pady = 10, sticky = E+W+N+S, columnspan=4)
-outputs.rowconfigure(0, weight=1)
-outputs.columnconfigure(0, weight=1)
+    # creating main tkinter window/toplevel 
+    master = Tk()
+    master.minsize(600, 600)
+    master.title("Ip Assignment 1")
+    master.iconphoto(False, PhotoImage(file='logo.png'))
 
-#Everything that goes the user inserts goes in the input frame
-l1 = Label(inputs, text = "IP ADDRESS:") 
-l2 = Label(inputs, text = "SUBNET MASK:")
-l3 = Label(inputs, text = "SUPERNET IP'S:")
+    master.columnconfigure(0, weight=1)
+    master.rowconfigure(1, weight=1)
 
-l1.grid(row = 0, column = 0, sticky = W, ipady = 2, ipadx = 2) 
-l2.grid(row = 1, column = 0, sticky = W, ipady = 2, ipadx= 2)
-l3.grid(row = 2, column = 0, sticky = W, ipady = 2, ipadx= 2)
+    #input frame
+    inputs = LabelFrame(master, text="Main Menu", padx=5, pady=5)
+    inputs.grid(row=0, column=0, padx=10, pady = 10, sticky = E+W+N+S, columnspan=4)
+    inputs.rowconfigure(0, weight=1)
+    inputs.columnconfigure(0, weight=1)
 
-e1 = Entry(inputs, width=30, bd=5) 
-e2 = Entry(inputs, width=30, bd=5)
-e3 = Entry(inputs, width=30, bd=5)
+    #output frame
+    outputs = LabelFrame(master, text="Output", padx=5, pady=5)
+    outputs.grid(row=1, column=0, padx=10, pady = 10, sticky = E+W+N+S, columnspan=4)
+    outputs.rowconfigure(0, weight=1)
+    outputs.columnconfigure(0, weight=1)
 
-e1.grid(row = 0, column = 1, pady = 2, sticky=E+W+N+S) 
-e2.grid(row = 1, column = 1, pady = 2, sticky=E+W+N+S)
-e3.grid(row = 2, column = 1, pady = 2, sticky=E+W+N+S)
+    #Everything that goes the user inserts goes in the input frame
+    l1 = Label(inputs, text = "IP ADDRESS:") 
+    l2 = Label(inputs, text = "SUBNET MASK:")
+    l3 = Label(inputs, text = "SUPERNET IP'S:")
 
-e1.insert(0,"e.g: (192.168.10.0)")
-e2.insert(0, "e.g: (255.255.255.192)")
-e3.insert(0, "e.g: 192.168.10.1,192.168.10.2,....")
+    l1.grid(row = 0, column = 0, sticky = W, ipady = 2, ipadx = 2) 
+    l2.grid(row = 1, column = 0, sticky = W, ipady = 2, ipadx= 2)
+    l3.grid(row = 2, column = 0, sticky = W, ipady = 2, ipadx= 2)
 
+    e1 = Entry(inputs, width=30, bd=5) 
+    e2 = Entry(inputs, width=30, bd=5)
+    e3 = Entry(inputs, width=30, bd=5)
 
+    e1.grid(row = 0, column = 1, pady = 2, sticky=E+W+N+S) 
+    e2.grid(row = 1, column = 1, pady = 2, sticky=E+W+N+S)
+    e3.grid(row = 2, column = 1, pady = 2, sticky=E+W+N+S)
 
-r = IntVar()
-c1 = Radiobutton(inputs, text="ip_stats", variable=r, value=1).grid(row = 0, column= 2, padx=2, pady=2, sticky=W)
-c2 = Radiobutton(inputs, text="subnet_stats", variable=r, value=2).grid(row = 1, column = 2, padx=2, pady=2, sticky=W)
-c3 = Radiobutton(inputs, text="supernet_stats", variable=r, value=3).grid(row = 2, column = 2, padx=2, pady=2, sticky=W)
-action = Button(inputs, text="Process",command=process_input).grid(row=3, column=1, sticky="nsew")
-quit = Button(inputs, text="Quit", command=master.quit, background="red", fg="white").grid(row=3, column=2, sticky="nsew", padx=10)
-
-
-#everything that will be going in the output frame
-
-
-
+    e1.insert(0,"e.g: (192.168.10.0)")
+    e2.insert(0, "e.g: (255.255.255.192)")
+    e3.insert(0, "e.g: 192.168.10.1,192.168.10.2,....")
 
 
-mainloop() 
+
+    r = IntVar()
+    c1 = Radiobutton(inputs, text="ip_stats", variable=r, value=1).grid(row = 0, column= 2, padx=2, pady=2, sticky=W)
+    c2 = Radiobutton(inputs, text="subnet_stats", variable=r, value=2).grid(row = 1, column = 2, padx=2, pady=2, sticky=W)
+    c3 = Radiobutton(inputs, text="supernet_stats", variable=r, value=3).grid(row = 2, column = 2, padx=2, pady=2, sticky=W)
+    action = Button(inputs, text="Process",command=process_input).grid(row=3, column=1, sticky="nsew")
+    quit = Button(inputs, text="Quit", command=master.quit, background="red", fg="white").grid(row=3, column=2, sticky="nsew", padx=10)
+
+
+    mainloop()
--- main.py
+++ main.py
@@ -1,57 +1,11 @@
-# import tkinter module 
-from tkinter import * 
-from tkinter import Entry
+#!usr/bin/env_python
 
-  
-# creating main tkinter window/toplevel 
-master = Tk()
-master.geometry("500x500")
-master.title("Ip Assignment 1")
-master.iconphoto(False, PhotoImage(file='logo.png'))
+import gui as g
 
 
-checks = LabelFrame(master, text="Output", background="black").grid()
 
+def main():
+    g.run()
 
-def stats():
-    ip = e1.get()
-    subnet = e2.get()
-    print(ip, subnet)
-
-#this will check if the value of the checkButtons
-def show():
-    myLabel1 = Label(master, text=var1.get()).grid(row=4, column=4)
-    mylabel2 = Label(master, text=var2.get()).grid(row=5, column=4)
-  
-# this wil create a label widget 
-l1 = Label(master, text = "IP ADDRESS:") 
-l2 = Label(master, text = "SUBNET MASK:") 
-  
-# grid method to arrange labels in respective 
-# rows and columns as specified 
-l1.grid(row = 0, column = 0, sticky = W, pady = 2, padx = 2) 
-l2.grid(row = 1, column = 0, sticky = W, pady = 2, padx= 2) 
-  
-# entry widgets, used to take entry from user 
-e1 = Entry(master, width=30, bd=5) 
-e2 = Entry(master, width=30, bd=5)
-
-
-# this will arrange entry widgets 
-e1.grid(row = 0, column = 1, pady = 2) 
-e2.grid(row = 1, column = 1, pady = 2)
-e1.insert(0,"e.g: (192.168.10.0)")
-e2.insert(0, "e.g: (255.255.255.192)")
-
-#this will arrange the checkboxes to enable different functions
-var1 = IntVar()
-var2 = IntVar()
-c1 = Checkbutton(checks, text="get_ip_stats", variable=var1).grid(row = 0, column= 2, padx=2, pady=2, sticky=W)
-c2 = Checkbutton(checks, text="get_subnet_stats", variable=var2).grid(row = 1, column = 2, padx=2, pady=2, sticky=W)
-myButton = Button(checks, text="test", command=show, width = 10).grid(row = 2, column=2)
-
-# infinite loop which can be terminated by keyboard 
-# or mouse interrupt 
-action = Button(master, text="Process", command=stats, width=10).grid(row=2, column=1, sticky=W)
-quit = Button(master, text="Quit", command=master.quit, width=10).grid(row=3, column=1, sticky=W)
-mainloop() 
+if __name__ == '__main__':
+    main()
